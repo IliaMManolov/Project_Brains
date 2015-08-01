cbbe07375599ababd3fb2c42f70fec3569be046a
Ilmanfordinner <ilmansonic@abv.bg>
2015-08-01 20:09:16 +0300

    Added Player and Bullet

---
+++ Project_Brains/Assets/Scripts/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour 
+{
+	public float despawnTime = 2f;
+
+
+	private float thyme;
+	void Start () 
+	{
+		thyme = Time.time;
+	}
+
+	void Update () 
+	{
+		if (Time.time - thyme > despawnTime)
+			Destroy (gameObject);
+	}
+}
