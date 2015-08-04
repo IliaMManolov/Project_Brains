d761630aad5de7e0c96ad119e48960d865f93760
Wang33 <petroverik@abv.bg>
2015-08-04 11:37:25 +0300

    Added Enemy and  EnemySpawner
    - Enemy can attack player
    - Every few seconds 1 enemy is spawned
    * Enemy movement is bugged
    * Enemy rotation doesnt work

--- Project_Brains/Assets/Scripts/EnemyDestroy.cs
+++ Project_Brains/Assets/Scripts/EnemyDestroy.cs
@@ -3,8 +3,66 @@ using System.Collections;
 
 public class EnemyDestroy : MonoBehaviour {
 
-	void OnTriggerEnter2D(Collider2D other) 
+	public int enemyHealth;
+	public int playerHealth;
+	public float attackTime;
+	public float lastAttack;
+	public int enemyDamage;
+	public int playerDamage;
+	public bool loadEnemyAttack = false;
+
+	public int speed;
+
+	Rigidbody2D rbEnemy;
+
+	public GameObject player;
+
+	void Start  ()
 	{
-		Debug.Log ("Hit!");
+		rbEnemy = GetComponent<Rigidbody2D>();
+	}
+
+	void Update ()
+	{
+		if (enemyHealth <= 0)
+		{
+			Destroy (gameObject);
+		}
+		if (playerHealth <= 0)
+		{
+			Debug.Log ("You are dead!");
+		}
+		//float angle = Mathf.Atan2(player.gameObject.transform.position.y - transform.position.y, player.gameObject.transform.position.x - transform.position.x) * 180 / Mathf.PI;
+
+		Vector2 velocity = new Vector2((transform.position.x - player.gameObject.transform.position.x) * speed, (transform.position.y - player.gameObject.transform.position.y) * speed);
+
+		rbEnemy.velocity = -velocity;
+	}
+
+	void OnTriggerEnter2D (Collider2D other) 
+	{
+		if (other.tag == "Bullet")
+		{
+			enemyHealth = enemyHealth - playerDamage;
+			Destroy (other.gameObject);
+		}
+
+	}
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			if (Time.time - lastAttack > attackTime)
+			{
+				Debug.Log ("Hit!");
+				loadEnemyAttack = true;
+			}
+			if (loadEnemyAttack == true)
+			{
+				playerHealth = playerHealth - enemyDamage;
+				loadEnemyAttack = false;
+				lastAttack = Time.time;
+			}
+		}
 	}
 }
---
+++ Project_Brains/Assets/Scripts/SpawnWave.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnWave : MonoBehaviour {
+
+	public GameObject enemy;
+	public int enemiesPerWave;
+	public int enemiesSpawned;
+	public int enemiesDead;
+	public float spawnStart;
+	public float spawnFixed;
+	
+	void Start () {
+	}
+
+	void Update () {
+		if (enemiesSpawned != enemiesPerWave) {
+			if (Time.time  >= spawnStart) {
+				SpawnEnemy ();
+				spawnStart = spawnStart + spawnFixed;
+				enemiesSpawned++;
+			}
+		}
+	}
+
+	void WaveSpawn () {
+
+	}
+	void SpawnEnemy () {
+
+		Instantiate (enemy, new Vector3 (-6F, 0, -3F), Quaternion.identity);
+	}
+}
